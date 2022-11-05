013b43edd09f4997c4a5b2b9937dda9346ded151
Anonymous <>
2022-11-05 12:37:54 -0400

    Add complete sentences.

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,7 +1,7 @@
-print('Hello')
-print('Another line')
-print('First line\nSecond Line')
-print("""Testing
-Triple
-Quotes""")
+print('Hello.')
+print('This is a full sentence.')
+print('This is the first line.\nThis is the second line.')
+print("""I am testing
+triple
+quotes""")
 
